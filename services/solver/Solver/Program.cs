69d01b3325a9a8d26d020815004347a4534201d8
Siddharth Bahekar <siddharthbahekar13@gmail.com>
2026-08-19 14:41:02 -0400

    feat: solver-core solves TSP with OR-Tools against hardcoded matrix

---
+++ services/solver/Solver/Program.cs
@@ -0,0 +1,48 @@
+﻿using Google.OrTools.ConstraintSolver;
+
+long[,] distanceMatrix =
+{
+    { 0, 10, 15, 20 },
+    { 10, 0, 35, 25 },
+    { 15, 35, 0, 30 },
+    { 20, 25, 30, 0 },
+};
+
+int vehicleCount = 1;
+int depot = 0;
+
+RoutingIndexManager manager = new RoutingIndexManager(
+    distanceMatrix.GetLength(0), vehicleCount, depot);
+
+RoutingModel routing = new RoutingModel(manager);
+
+int transitCallbackIndex = routing.RegisterTransitCallback((long fromIndex, long toIndex) =>
+{
+    var fromNode = manager.IndexToNode(fromIndex);
+    var toNode = manager.IndexToNode(toIndex);
+    var cost = distanceMatrix[fromNode, toNode];
+    Console.WriteLine($"callback: {fromNode}->{toNode} = {cost}");
+    return cost;
+});
+
+routing.SetArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
+
+RoutingSearchParameters searchParameters =
+    operations_research_constraint_solver.DefaultRoutingSearchParameters();
+searchParameters.FirstSolutionStrategy =
+    FirstSolutionStrategy.Types.Value.PathCheapestArc;
+
+Assignment solution = routing.SolveWithParameters(searchParameters);
+
+var index = routing.Start(0);
+Console.Write("Route: ");
+long routeCost = 0;
+while (!routing.IsEnd(index))
+{
+    Console.Write($"{manager.IndexToNode(index)} -> ");
+    var previousIndex = index;
+    index = solution.Value(routing.NextVar(index));
+    routeCost += routing.GetArcCostForVehicle(previousIndex, index, 0);
+}
+Console.WriteLine(manager.IndexToNode(index));
+Console.WriteLine($"Route cost: {routeCost}");
