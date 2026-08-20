d80fee733663d3410bb1e8cf0b0481dc19c7af9d
Siddharth Bahekar <siddharthbahekar13@gmail.com>
2026-08-20 19:38:38 -0400

    test: add xUnit project, refactor solver into RouteSolver, assert optimal cost

---
+++ services/solver/Solver.Tests/RouteSolverTests.cs
@@ -0,0 +1,27 @@
+﻿using Solver;
+using Xunit;
+
+namespace Solver.Tests;
+
+public class RouteSolverTests
+{
+    [Fact]
+    public void Solve_FourStopMatrix_ReturnsOptimalCost()
+    {
+        // Arrange
+        long[,] matrix =
+        {
+            { 0, 10, 15, 20 },
+            { 10, 0, 35, 25 },
+            { 15, 35, 0, 30 },
+            { 20, 25, 30, 0 },
+        };
+        var solver = new RouteSolver();
+
+        // Act
+        var result = solver.Solve(matrix);
+
+        // Assert
+        Assert.Equal(80, result.TotalCost);
+    }
+}
